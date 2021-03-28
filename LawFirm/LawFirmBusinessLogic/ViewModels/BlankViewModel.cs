7f082915cc63c8d3d4e88f661091cf7939b9bad1
Сергей Резников <tikto01@mail.ru>
2021-03-28 17:20:17 +0400

    first laboratory is copied

---
+++ LawFirm/LawFirmBusinessLogic/ViewModels/BlankViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace LawFirmBusinessLogic.ViewModels
+{
+    public class BlankViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Название бланка")]
+        public string BlankName { get; set; }
+    }
+}
---
+++ LawFirm/LawFirmListImplements/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using LawFirmBusinessLogic.Enums;
+using System;
+namespace LawFirmListImplement.Models
+{
+    /// <summary>
+    /// Заказ
+    /// </summary>
+    public class Order
+    {
+        public int Id { get; set; }
+        public int DocumentId { get; set; }
+        public decimal Sum { set; get; }
+        public int Count { get; set; }
+        public OrderStatus Status { get; set; }
+        public DateTime DateCreate { get; set; }
+        public DateTime? DateImplement { get; set; }
+    }
+}
