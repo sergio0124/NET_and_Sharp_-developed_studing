8e4060c2df91a9856b283496b91addbc5815fd32
SergeyReznikov <tikto01@mail.ru>
2021-04-10 15:47:50 +0400

    copied

---
+++ LawFirm/LawFirm/FormStorage.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace LawFirmView
+{
+    partial class FormStorage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.BlankId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Blank = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBoxManager = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelManager = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BlankId,
+            this.Blank,
+            this.Count});
+            this.dataGridView.Location = new System.Drawing.Point(12, 73);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.Size = new System.Drawing.Size(380, 283);
+            this.dataGridView.TabIndex = 26;
+            // 
+            // BlankId
+            // 
+            this.BlankId.HeaderText = "";
+            this.BlankId.MinimumWidth = 6;
+            this.BlankId.Name = "BlankId";
+            this.BlankId.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.BlankId.Visible = false;
+            // 
+            // Blank
+            // 
+            this.Blank.HeaderText = "Бланк";
+            this.Blank.MinimumWidth = 6;
+            this.Blank.Name = "Blank";
+            // 
+            // Count
+            // 
+            this.Count.HeaderText = "Количество";
+            this.Count.MinimumWidth = 6;
+            this.Count.Name = "Count";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(317, 363);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 30;
+            this.buttonCancel.Text = "Отмена";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(236, 363);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 29;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBoxManager
+            // 
+            this.textBoxManager.Location = new System.Drawing.Point(104, 34);
+            this.textBoxManager.Name = "textBoxManager";
+            this.textBoxManager.Size = new System.Drawing.Size(188, 20);
+            this.textBoxManager.TabIndex = 32;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(104, 8);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(188, 20);
+            this.textBoxName.TabIndex = 31;
+            // 
+            // labelManager
+            // 
+            this.labelManager.AutoSize = true;
+            this.labelManager.Location = new System.Drawing.Point(12, 34);
+            this.labelManager.Name = "labelManager";
+            this.labelManager.Size = new System.Drawing.Size(89, 13);
+            this.labelManager.TabIndex = 28;
+            this.labelManager.Text = "Ответственный:";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(12, 12);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(60, 13);
+            this.labelName.TabIndex = 27;
+            this.labelName.Text = "Название:";
+            // 
+            // FormStorage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 396);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxManager);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelManager);
+            this.Controls.Add(this.labelName);
+            this.Name = "FormStorage";
+            this.Text = "FormStorage";
+            this.Load += new System.EventHandler(this.FormStorage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BlankId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Blank;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Count;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBoxManager;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelManager;
+        private System.Windows.Forms.Label labelName;
+    }
+}
